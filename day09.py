ac6736ed4c4c6a29fc67552234903634f910ba60
Oguzhan Selcuk <me@oselcuk.com>
2023-12-09 16:18:40 +0100

    Day 9 part 2

--- day09.py
+++ day09.py
@@ -2,11 +2,25 @@ from itertools import pairwise
 
 def part1(lines):
     seqs = (list(map(int, line.split())) for line in lines)
-    return sum(map(extrapolate, seqs))
+    return sum(map(extrapolate_next, seqs))
 
-def extrapolate(seq: list[int]) -> int:
+
+def part2(lines):
+    seqs = (list(map(int, line.split())) for line in lines)
+    return sum(map(extrapolate_prev, seqs))
+
+
+def diffs(seq: list[int]) -> list[int]:
+    return list(map(lambda pair: pair[1] - pair[0], pairwise(seq)))
+
+
+def extrapolate_next(seq: list[int]) -> int:
     if not any(seq):
         return 0
-    diffs = list(map(lambda pair: pair[1] - pair[0], pairwise(seq)))
-    return seq[-1] + extrapolate(diffs)
-    
+    return seq[-1] + extrapolate_next(diffs(seq))
+
+
+def extrapolate_prev(seq: list[int]) -> int:
+    if not any(seq):
+        return 0
+    return seq[0] - extrapolate_prev(diffs(seq))
