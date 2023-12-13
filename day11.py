c06512debb80519c5362991419043087bcf70f8c
Oguzhan Selcuk <me@oselcuk.com>
2023-12-13 20:45:17 +0100

    Day 11 part 2

--- day11.py
+++ day11.py
@@ -2,7 +2,8 @@ from typing import Iterable
 from bisect import bisect_left
 import itertools
 
-def part1(lines: Iterable[str]) -> int:
+
+def solve(lines: Iterable[str], expansion_factor: int) -> int:
     sky = [[c == "#" for c in row.strip()] for row in lines]
     empty_rows = [i for i, row in enumerate(sky) if not any(row)]
     transposed = list(zip(*sky))
@@ -11,5 +12,13 @@ def part1(lines: Iterable[str]) -> int:
     for i, row in enumerate(sky):
         for j, c in enumerate(row):
             if c:
-                galaxies.add((i + bisect_left(empty_rows, i), j + bisect_left(empty_cols, j)))
+                galaxies.add((i + bisect_left(empty_rows, i) * expansion_factor, j + bisect_left(empty_cols, j) * expansion_factor))
     return sum(abs(x0-x1) + abs(y0-y1) for ((x0, y0), (x1, y1)) in itertools.combinations(galaxies, 2))
+
+
+def part1(lines):
+    return solve(lines, 1)
+
+
+def part2(lines):
+    return solve(lines, 1000000 - 1)
